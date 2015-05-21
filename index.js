51846556a488857fcc9fd026da8ed7133a4aad51
Peter Helenefors <peter@hertigcarl.se>
2015-05-21 23:20:45 +0200

    added removal of disconnected user

--- index.js
+++ index.js
@@ -4,10 +4,17 @@ var WebSocketServer = require('ws').Server
 wss.on('connection', function(ws) {
 	var player = ws.upgradeReq.url.substring(1);
 	players[player] = ws;
+	console.log(Object.keys(players).length);
 
     ws.on('message', function(message) {
         console.log('received: %s', message);
     });
 
     ws.send('Welcome ' + player);
+
+    ws.on('close', function() {
+    	console.log('men satan!');
+    	delete players[ws.upgradeReq.url.substring(1)];
+    	console.log(Object.keys(players).length);
+    });
 });
